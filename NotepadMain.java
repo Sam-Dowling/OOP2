92fa3ce26b96168805911c1b0b230e9640ede085
unknown <t00171832@1XX782BCBAC2827.students.ittralee.local>
2014-12-08 14:38:28 +0000

    renamed files

---
+++ NotepadMain.java
@@ -0,0 +1,11 @@
+// NotepadMain.java
+/**
+ * Sam Dowling | T00171832
+ * 08/12/2014
+ */
+public class NotepadMain{
+	public static void main(String[] args){
+    	Notepad pad = new Notepad();
+    	pad.setVisible(true);
+    }
+}
